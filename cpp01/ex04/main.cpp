1edb7d4f901cec961e89e48cdb2b244b8787a7d5
Jeongwoo Son <jeson@c2r4s6.42seoul.kr>
2022-02-02 18:09:46 +0900

    cpp01-04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+// int main(int argc, char **argv)
+// {
+//     std::string extend = ".replace";
+//     std::string replace;
+//     std::ofstream fout;
+
+//     if (argc != 4)
+//         return (1);
+//     if (!argv[2] || !argv[3])
+//         return (1);
+//     fout.open(const char* argv[1]);
+//     for (int i = 0; i <= 10; i++)
+//         fout << i << "\n";
+//     fout << std::endl;
+//     fout.close();
+//     // open (const char* replace = argv[1] + extend, ios_base::openmode mode = ios_base::out);
+//     return (0);
+// }
