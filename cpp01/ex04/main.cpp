bfea5b4eabdfb8fc10575039cd2b94bdfe90a69b
JeongWoo <jeson@JeongWoos-MacBook-Air.local>
2022-02-05 15:09:24 +0900

    cpp01-04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jeson <jeson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 14:33:38 by jeson             #+#    #+#             */
-/*   Updated: 2022/02/05 14:33:39 by jeson            ###   ########.fr       */
+/*   Updated: 2022/02/05 15:09:10 by jeson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,28 @@
 #include <string>
 #include <fstream>
 
-// int main(int argc, char **argv)
-// {
-//     std::string extend = ".replace";
-//     std::string replace;
-//     std::ofstream fout;
+int main(int argc, char **argv)
+{
+    std::string extend = ".replace";
+    std::string replace;
+	char	c;
 
-//     if (argc != 4)
-//         return (1);
-//     if (!argv[2] || !argv[3])
-//         return (1);
-//     fout.open(const char* argv[1]);
-//     for (int i = 0; i <= 10; i++)
-//         fout << i << "\n";
-//     fout << std::endl;
-//     fout.close();
-//     // open (const char* replace = argv[1] + extend, ios_base::openmode mode = ios_base::out);
-//     return (0);
-// }
+    if (argc != 4)
+        return (1);
+    if (!argv[2] || !argv[3])
+        return (1);
+    std::ifstream fin(argv[1]);
+	std::ofstream fre(argv[1] + extend);
+	if (!fin.is_open() || !fre.is_open())
+		return (1);
+	while (fin.get(c))
+	{
+		if (c == *argv[2])
+			fre.write(&*argv[3], 1);
+		else
+			fre.write(&c, 1);
+	}
+    fin.close();
+	fre.close();
+    return (0);
+}
