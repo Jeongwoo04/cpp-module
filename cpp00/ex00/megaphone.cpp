3d66a457f1acdd056bdccb09f7662f8e9b4fde3a
Jeongwoo Son <jeson@c1r2s5.42seoul.kr>
2022-02-06 13:01:11 +0900

    cpp00 check

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -23,10 +23,8 @@ char	ft_toupper(char ch)
 
 int	main(int argc, char **argv)
 {
-	int	i;
-
 	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	argv++;
 	while (*argv)
 	{
