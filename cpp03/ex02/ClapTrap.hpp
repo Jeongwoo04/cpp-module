73635a1a0d84c70b3d98160854fb75e2fd34514b
Jeongwoo Son <jeson@c2r8s5.42seoul.kr>
2022-02-09 19:43:18 +0900

    cpp03-02

---
+++ cpp03/ex02/ClapTrap.hpp
@@ -0,0 +1,28 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+class ClapTrap
+{
+protected:
+    std::string name;
+    unsigned int hitpoint;
+    unsigned int energy;
+    unsigned int damage;
+public:
+    ClapTrap();
+    ClapTrap( std::string name );
+    ClapTrap(const ClapTrap& claptrap);
+    ~ClapTrap();
+
+    ClapTrap& operator=(const ClapTrap& claptrap);
+    
+    void attack(std::string const & target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    unsigned int getDamage( void ) const;
+};
+
+#endif
---
+++ cpp03/ex02/FragTrap.cpp
@@ -0,0 +1,52 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap()
+{
+    name = "stranger";
+    hitpoint = 100;
+    energy = 100;
+    damage = 30;
+    std::cout << name << " : FragTrap default constructor is called" << std::endl;
+};
+
+FragTrap::FragTrap( std::string str )
+{
+    name = str;
+    hitpoint = 100;
+    energy = 100;
+    damage = 30;
+    std::cout << name << " : FragTrap string constructor is called" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& fragtrap)
+{
+    std::cout << name << " : FragTrap copy constructor is called" << std::endl;
+    *this = fragtrap;
+}
+
+FragTrap::~FragTrap()
+{
+    std::cout << name << " : FragTrap destructor is called" << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& fragtrap)
+{
+    std::cout << name << " : FragTrap operator is called" << std::endl;
+    if (this != &fragtrap)
+    {
+        *this = fragtrap;
+        return (*this);
+    }
+    return (*this);
+}
+
+void    FragTrap::attack( std::string const& target )
+{
+    std::cout << "[FragTrap]" << std::endl;
+    std::cout << name << " attack " << target << ", causing " << damage << " points of damage!" << std::endl;
+}
+
+void	FragTrap::highFivesGuys(void)
+{
+	std::cout << name << " : FragTrap is called positive high fives request" << std::endl;
+}
