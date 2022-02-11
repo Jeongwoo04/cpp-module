7accdc371de92f24a388b073a14c3590ce10583f
Jeongwoo Son <jeson@c4r2s8.42seoul.kr>
2022-02-11 19:13:25 +0900

    cpp03-03

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -2,16 +2,16 @@
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-    float alpha =   ((b.getY() - c.getY())*(point.getX() - c.getX()) +
-                    (c.getX() - b.getX())*(point.getY() - c.getY())) / 
-				    ((b.getY() - c.getY())*(a.getX() - c.getX()) +
-                    (c.getX() - b.getX())*(a.getY() - c.getY())); 
-    float beta =    ((c.getY() - a.getY())*(point.getX() - c.getX()) +
-                    (a.getX() - c.getX())*(point.getY() - c.getY())) / 
-				    ((b.getY() - c.getY())*(a.getX() - c.getX()) +
-                    (c.getX() - b.getX())*(a.getY() - c.getY())); 
-    float gamma = 1.0f - alpha - beta;
-    if (alpha > 0 && beta > 0 && gamma > 0)
-        return ( true );
-    return ( false );
+	float alpha	=	((b.getY() - c.getY())*(point.getX() - c.getX()) +
+					(c.getX() - b.getX())*(point.getY() - c.getY())) /
+					((b.getY() - c.getY())*(a.getX() - c.getX()) +
+					(c.getX() - b.getX())*(a.getY() - c.getY()));
+	float beta	=	((c.getY() - a.getY())*(point.getX() - c.getX()) +
+					(a.getX() - c.getX())*(point.getY() - c.getY())) /
+					((b.getY() - c.getY())*(a.getX() - c.getX()) +
+					(c.getX() - b.getX())*(a.getY() - c.getY()));
+	float gamma	=	1.0f - alpha - beta;
+	if (alpha > 0 && beta > 0 && gamma > 0)
+		return ( true );
+	return ( false );
 }
