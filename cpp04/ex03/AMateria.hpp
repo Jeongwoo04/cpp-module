5e4577ce8481ff38bcb783776820a38cacbe9515
Jeongwoo Son <jeson@c2r6s7.42seoul.kr>
2022-02-14 19:52:24 +0900

    cpp04-0214

--- cpp04/ex03/AMateria.hpp
+++ cpp04/ex03/AMateria.hpp
@@ -6,11 +6,22 @@
 /*   By: jeson <jeson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 18:31:24 by jeson             #+#    #+#             */
-/*   Updated: 2022/02/14 18:31:45 by jeson            ###   ########.fr       */
+/*   Updated: 2022/02/14 19:06:00 by jeson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AMATERIA_HPP
 # define AMATERIA_HPP
+
+class AMateria
+{
+protected:
+
+public:
+	AMateria( std::string const & type );
+	std::string const & getType() const; //Returns the materia type
+	virtual AMateria* clone() const = 0;
+	virtual void use(ICharacter& target);
+};
 
 #endif
