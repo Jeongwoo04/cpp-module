04033b22b7f44672de0c4a97061c36b9840345f2
JeongWoo <jeson@JeongWoos-MacBook-Air.local>
2022-02-16 22:31:44 +0900

    cpp05-02

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jeson <jeson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 15:23:04 by jeson             #+#    #+#             */
-/*   Updated: 2022/02/16 22:15:39 by jeson            ###   ########.fr       */
+/*   Updated: 2022/02/16 22:31:02 by jeson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,49 @@
 
 int	main( void )
 {
+	std::cout << "============= new form create =============" << std::endl;
 	Form *sform = new ShrubberyCreationForm("shrubbery");
 	Form *rform = new RobotomyRequestForm("robotomy");
 	Form *pform = new PresidentialPardonForm("presidential");
 	std::cout << std::endl;
 
+	std::cout << "============= bureaucrat create =============" << std::endl;
 	Bureaucrat jeson("jeson", 150);
 	std::cout << jeson << std::endl;
 
+	std::cout << "============= excuteForm before signed form =============" << std::endl;
 	jeson.executeForm(*pform);
 	std::cout << std::endl;
 
-	while (jeson.getGrade() > 5)
+	while (jeson.getGrade() > 10)
 		jeson.incrementGrade();
 	jeson.executeForm(*pform);
 	std::cout << std::endl;
 
+	std::cout << "============= excuteForm after signed form =============" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "============= sign pform . . ? =============" << std::endl;
 	jeson.signForm(*pform);
 	std::cout << std::endl;
 
+	std::cout << "============= pForm excutable !? =============" << std::endl;
 	jeson.executeForm(*pform);
-
 	std::cout << std::endl;
+
+	std::cout << "============= sign rform . . ? =============" << std::endl;
 	jeson.signForm(*rform);
-
 	std::cout << std::endl;
-	jeson.executeForm(*rform);
 
-	std::cout << "\n";
+	std::cout << "============= rForm excutable !? =============" << std::endl;
+	jeson.executeForm(*rform);
+	std::cout << std::endl;
+
+	std::cout << "============= sign sform . . ? =============" << std::endl;
 	jeson.signForm(*sform);
 	std::cout << std::endl;
+
+	std::cout << "============= sForm excutable !? =============" << std::endl;
 	jeson.executeForm(*sform);
 	return ( 0 );
 }
